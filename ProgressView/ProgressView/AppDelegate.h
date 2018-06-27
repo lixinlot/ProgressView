0ffeb52df5caf5cadeef8818702cf044f86f2fae
lixinlot <lixinlot@126.com>
2018-06-27 22:54:18 +0800

    创建项目

---
+++ ProgressView/ProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgressView
+//
+//  Created by jimmy on 2018/6/27.
+//  Copyright © 2018年 jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgressView/ProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressView
+//
+//  Created by jimmy on 2018/6/27.
+//  Copyright © 2018年 jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
